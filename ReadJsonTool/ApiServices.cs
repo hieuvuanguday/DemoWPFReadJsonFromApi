dc10f1d5e8756c248de6884682ee42471649542d
hieuvuanguday <dothehieu.92.20172018@gmail.com>
2024-11-02 16:35:26 +0700

    Initial commit

---
+++ ReadJsonTool/ApiServices.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadJsonTool
+{
+    public class ApiServices
+    {
+        public async Task<List<User>> GetUsersAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/posts");
+                HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<List<User>>(jsonData);
+                }
+
+                return new List<User>();
+            }
+        }
+    }
+}
---
+++ ReadJsonTool/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReadJsonTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ApiServices _apiServices = new ApiServices();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void UsersDataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var list = await _apiServices.GetUsersAsync();
+            UsersDataGrid.ItemsSource = list;
+        }
+    }
+}
